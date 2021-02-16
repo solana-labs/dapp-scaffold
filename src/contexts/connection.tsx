bd0d7300b13b342224769ecfdbdcba516c0def54
bartosz-lipinski <264380+bartosz-lipinski@users.noreply.github.com>
2021-02-16 12:18:05 -0600

    fix: connections

--- src/contexts/connection.tsx
+++ src/contexts/connection.tsx
@@ -18,22 +18,12 @@ export type ENV =
   | "mainnet-beta"
   | "testnet"
   | "devnet"
-  | "localnet"
-  | "lending";
+  | "localnet";
 
 export const ENDPOINTS = [
   {
     name: "mainnet-beta" as ENV,
     endpoint: "https://solana-api.projectserum.com/",
-  },
-  {
-    name: "Oyster Dev" as ENV,
-    endpoint: "http://oyster-dev.solana.com/",
-  },
-  {
-    name: "Lending" as ENV,
-    endpoint: "https://tln.solana.com/",
-  },
   { name: "testnet" as ENV, endpoint: clusterApiUrl("testnet") },
   { name: "devnet" as ENV, endpoint: clusterApiUrl("devnet") },
   { name: "localnet" as ENV, endpoint: "http://127.0.0.1:8899" },
@@ -95,7 +85,7 @@ export function ConnectionProvider({ children = undefined as any }) {
     // fetch token files
     window
       .fetch(
-        `https://raw.githubusercontent.com/solana-labs/token-list/main/src/tokens/${env}.json`
+        `https://raw.githubusercontent.com/project-serum/serum-ts/master/packages/tokens/src/${env}.json`
       )
       .then((res) => {
         return res.json();
