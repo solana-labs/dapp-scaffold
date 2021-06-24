cecbf58e8fc7645b84b20abbfb46c5fe2059cb3b
Edward Bramanti <edward@bramanti.org>
2021-06-24 01:50:14 -0600

    Fix Jest test runner - Update @project-serum/sol-wallet-adapter to latest   * Fixes superstruct dependency mismatch - Remove manual types - Fix typings in Jest tests   * Load typeRoots from node_modules - Add global mocks for testing-library rendering - Update test to check for text that exists

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+jest.mock("./components/Identicon", () => {
+  return {
+    __esModule: true,
+    Identicon: () => null,
+  };
+});
+
+test("renders balances text", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Your balances/i);
   expect(linkElement).toBeInTheDocument();
 });
