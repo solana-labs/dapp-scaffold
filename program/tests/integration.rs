4fdacf9e7c58a5db81a4ab9e89cdfd7577ccb36a
chris <christophpryer@gmail.com>
2021-09-11 13:28:59 -0400

    resolved cargo fmt fail

--- program/tests/integration.rs
+++ program/tests/integration.rs
@@ -1,12 +1,12 @@
 use {
-    bpf_program_template::process_instruction,
     assert_matches::assert_matches,
+    bpf_program_template::process_instruction,
     solana_program::{
         instruction::{AccountMeta, Instruction},
         pubkey::Pubkey,
     },
+    solana_program_test::{processor, tokio, ProgramTest},
     solana_sdk::{signature::Signer, transaction::Transaction},
-    solana_program_test::{processor, tokio, ProgramTest}
 };
 
 #[tokio::test]
