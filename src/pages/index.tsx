e4adea39bac67f67e35fdcf074b273d78d56db07
cwlee <cwlee0@gmail.com>
2022-03-15 14:28:48 +0800

    fix: solana embed in nextjs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { HomeView } from "../views";
+// import Torus from "@toruslabs/solana-embed";
+import { useEffect } from "react";
 
 const Home: NextPage = (props) => {
+  useEffect(()=>{
+    const init = async () => {
+        const Torus = (await import("@toruslabs/solana-embed")).default
+        console.log(Torus)
+        let torus = new Torus({})
+        await torus.init()
+        
+        await torus.login()
+    }
+    init();
+  },[])
   return (
     <div>
       <Head>
@@ -11,7 +24,8 @@ const Home: NextPage = (props) => {
           name="description"
           content="Solana Scaffold"
         />
-      </Head>
+      </Head>    
+
       <HomeView />
     </div>
   );
