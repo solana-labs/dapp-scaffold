306bae4bcc915d80b51560e6a32b12a82a48a897
Dan Baker <danielebaker@gmail.com>
2022-04-20 09:55:18 -0400

    fix3

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -46,16 +46,18 @@ export async function getRpcUrl(): Promise<string> {
  * Load and parse the Solana CLI config file to determine which payer to use
  */
 export async function getPayer(): Promise<Keypair> {
-  try {
-    const config = await getConfig();
-    if (!config.keypair_path) throw new Error('Missing keypair path');
-    return await createKeypairFromFile(config.keypair_path);
-  } catch (err) {
-    console.warn(
-      'Failed to create keypair from CLI config file, falling back to new random keypair',
-    );
-    return Keypair.generate();
-  }
+  return Keypair.generate();
+
+  // try {
+  //   const config = await getConfig();
+  //   if (!config.keypair_path) throw new Error('Missing keypair path');
+  //   return await createKeypairFromFile(config.keypair_path);
+  // } catch (err) {
+  //   console.warn(
+  //     'Failed to create keypair from CLI config file, falling back to new random keypair',
+  //   );
+  //   return Keypair.generate();
+  // }
 }
 
 /**
