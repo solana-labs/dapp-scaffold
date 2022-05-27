a204961b8dfa33e78d246c8bdff3ede1242aec36
IshaMandaviya <isha@solulab.com>
2022-05-27 15:38:21 +0530

    created components for Buy

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -3,24 +3,26 @@ import { FC } from "react";
 // import { SignMessage } from '../../components/SignMessage';
 // import { CreateNFTS } from '../../components/CreateNFTS';
 // import { FetchNFTS} from '../../components/FetchNFTS';
-import {CreateAuctionHouse} from '../../AuctionHouseComponents/CreateAuctionHouse';
-import {Sell} from '../../AuctionHouseComponents/Sell';
-import {GetAuctionHouse} from '../../AuctionHouseComponents/GetAuctionHouse';
-import {ExecuteSell} from '../../AuctionHouseComponents/ExecuteSell';
-export const HomeView : FC = ({ }) => {
+import { CreateAuctionHouse } from '../../AuctionHouseComponents/CreateAuctionHouse';
+import { Sell } from '../../AuctionHouseComponents/Sell';
+import { GetAuctionHouse } from '../../AuctionHouseComponents/GetAuctionHouse';
+import { ExecuteSell } from '../../AuctionHouseComponents/ExecuteSell';
+import { Buy } from '../../AuctionHouseComponents/Buy';
+export const HomeView: FC = ({ }) => {
 
   return (
-<div className="md:hero mx-auto p-4">
+    <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
           Auction House
         </h1>
         {/* CONTENT GOES HERE */}
         <div className="text-center">
-         <CreateAuctionHouse/>
-         <GetAuctionHouse/>
-         <Sell/>
-         <ExecuteSell/>
+          <CreateAuctionHouse />
+          <GetAuctionHouse />
+          <Sell />
+          <Buy />
+          <ExecuteSell />
 
         </div>
       </div>
