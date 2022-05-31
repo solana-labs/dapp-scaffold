b2f779c0c411edfc57f5880be1f56c99771772ec
Abhishek Faliya <57398733+Maverick9081@users.noreply.github.com>
2022-05-31 13:39:49 +0530

    added necessary exports

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,3 +1,6 @@
 export { HomeView } from "./home";
 export { BasicsView } from "./basics";
+export { BuyView } from "./buy";
+export { SellView } from "./sell";
+export { ExecuteView } from "./execute";
 
