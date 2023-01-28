a423c44fcd3dec30c3c77f4ee204e4c6e1a37988
C <caaarloos300@gmail.com>
2023-01-29 02:17:47 +0900

    adding blur border effect + solving mobile issues

--- src/components/RequestAirdrop.tsx
+++ src/components/RequestAirdrop.tsx
@@ -35,19 +35,22 @@ export const RequestAirdrop: FC = () => {
     }, [publicKey, connection, getUserSOLBalance]);
 
     return (
-        <div>
-                 <div className="relative group">
-                                <div className="absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-fuchsia-500 rounded-lg blur opacity-20 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+
+        <div className="flex flex-row justify-center">
+                <div className="relative group items-center">
+                    <div className="m-1 absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-fuchsia-500 rounded-lg blur opacity-20 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
             
-                <button
-                className="px-8 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
-                                    onClick={onClick}
-                 >
-                    <span>Airdrop 1 </span>
+                        <button
+                            className="px-8 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
+                            onClick={onClick}
+                            >
+                                <span>Airdrop 1 </span>
                 
-                </button>
+                        </button>
                 </div>
         </div>
+
+        
     );
 };
 
--- src/components/SendVersionedTransaction.tsx
+++ src/components/SendVersionedTransaction.tsx
@@ -55,18 +55,21 @@ export const SendVersionedTransaction: FC = () => {
     }, [publicKey, notify, connection, sendTransaction]);
 
     return (
-        <div>
-            <button
-                className="group w-60 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
-                onClick={onClick} disabled={!publicKey}
-            >
-                <div className="hidden group-disabled:block ">
-                    Wallet not connected
-                </div>
-                <span className="block group-disabled:hidden" >
-                    Send Versioned Transaction
-                </span>
-            </button>
+        <div className="flex flex-row justify-center">
+            <div className="relative group items-center">
+                <div className="m-1 absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-fuchsia-500 rounded-lg blur opacity-20 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+                    <button
+                        className="group w-60 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
+                        onClick={onClick} disabled={!publicKey}
+                        >
+                    <div className="hidden group-disabled:block ">
+                        Wallet not connected
+                    </div>
+                    <span className="block group-disabled:hidden" >
+                        Send Versioned Transaction
+                    </span>
+                    </button>
+            </div>
         </div>
     );
 };
