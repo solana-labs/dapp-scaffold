23cc85da4109779f011a191d7351e03e2d53fa3c
C <caaarloos300@gmail.com>
2023-01-30 23:46:47 +0900

    hide hamburguer menu when bigger screens

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -20,7 +20,7 @@ export const AppBar: React.FC = () => {
       {/* NavBar / Header */}
       <div className="navbar flex h-20 flex-row md:mb-2 shadow-lg bg-black text-neutral-content border-b border-zinc-600">
         <div className="navbar-start">
-          <label htmlFor="my-drawer" className="btn btn-square btn-ghost">
+          <label htmlFor="my-drawer" className="btn btn-square btn-ghost items-center justify-between md:hidden">
 
             <svg className="inline-block w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" >
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
