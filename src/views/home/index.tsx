6d441f295f2036994321ff5281105d009a17fd73
DonnySolana <79116664+DonnySolana@users.noreply.github.com>
2023-01-31 01:44:49 -0800

    +update-npx-cmd-dapp-name

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -44,7 +44,7 @@ export const HomeView: FC = ({ }) => {
           <div className="absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-indigo-500 rounded-lg blur opacity-40 animate-tilt"></div>
           <div className="max-w-md mx-auto mockup-code bg-primary border-2 border-[#5252529f] p-6 px-10 my-2">
             <pre data-prefix=">">
-              <code className="truncate">npx create-solana-dapp  </code>
+              <code className="truncate">{`npx create-solana-dapp <dapp-name>`} </code>
             </pre>
           </div>
         </div>
