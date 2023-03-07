17cfaac4fbe2b51056d1a431d43b0d694985f09c
pmpwith2i <federico.pomponii@gmail.com>
2023-03-07 17:44:02 +0100

    chore: setting eslint and prettier

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+// prettier.config.js
+module.exports = {
+  // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+  plugins: [require('prettier-plugin-tailwindcss')],
+  singleQuote: true,
+  jsxSingleQuote: true,
+};
