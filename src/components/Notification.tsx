1dbc9bc20f7b37a2a8f37c0f9c7a2d0aa5690f78
YTW7 <91966435+YTW7@users.noreply.github.com>
2023-05-20 07:15:15 +0000

    updated color of the url link

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -91,7 +91,7 @@ const Notification = ({ type, message, description, txid, onHide }) => {
                   href={'https://explorer.solana.com/tx/' + txid + `?cluster=${networkConfiguration}`}
                   target="_blank"
                   rel="noreferrer"
-                  className="flex flex-row link link-accent"
+                  className="flex flex-row link link-accent text-emerald-200"
                 >
                   <svg className="flex-shrink-0 h-4 ml-2 mt-0.5 text-primary-light w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" ><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg>
                   <div className="flex mx-4">{txid.slice(0, 8)}...
