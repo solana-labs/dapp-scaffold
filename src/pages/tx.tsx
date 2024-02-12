bd60123df3cf1b5a7dca461c46d970d827d2f8bc
Eric Debelak <eric@flip.xyz>
2024-02-12 11:38:34 -0700

    Add reworked tx screen

--- src/pages/tx.tsx
+++ src/pages/tx.tsx
@@ -1,58 +1,62 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
-import { mplCandyMachine, create, fetchCandyMachine, fetchCandyGuard, addConfigLines, mintV2, route, deleteCandyGuard, deleteCandyMachine, updateCandyGuard } from '@metaplex-foundation/mpl-candy-machine'
-import { Connection } from "@solana/web3.js"
+import { CandyMachineV2, Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { PublicKey } from "@solana/web3.js";
 
 const Tx = () => {
-    // Config
-    // main net
-    const rpcConnection = new Connection(process.env.NEXT_PUBLIC_MAINNET_ENDPOINT)
+    const { connection } = useConnection();
+    const wallet = useWallet();
+    const [candyMachine, setCandyMachine] = useState<CandyMachineV2 | undefined>(undefined)
+    const [isMinting, setIsMinting] = useState(false)
 
-    // test net
-    // const rpcConnection = new Connection(process.env.NEXT_PUBLIC_TESTNET_ENDPOINT)
-    const umi = createUmi(rpcConnection)
+    const metaplex = Metaplex
+        .make(connection)
+        .use(walletAdapterIdentity(wallet))
 
-    // Umi config
-    umi.use(mplCandyMachine());
+    const candyMachines = metaplex.candyMachinesV2()
 
-    const fetchCandyMachineAndGuard = async (candyMachinePublicKey, doLog) => {
-        const candyMachine = await fetchCandyMachine(umi, candyMachinePublicKey)
-        const candyGuard = await fetchCandyGuard(umi, candyMachine.mintAuthority)
-    
-        if (doLog) {
-            console.log(`Candy machine public key: ${candyMachine.publicKey}`)
-            console.log(`Candy machine mint authority: ${candyMachine.mintAuthority}`)
-            console.log(`Candy machine items available: ${candyMachine.itemsAvailable}`)
-            console.log(`Candy machine items redeemed: ${candyMachine.itemsRedeemed}`)
-            console.log(`Candy machine items loaded: ${candyMachine.itemsLoaded}`)
-            console.log("Items:")
-            console.log(candyMachine.items)
-            console.log(`Candy machine authority: ${candyMachine.authority}`)
-            console.log(`Candy guard public key: ${candyGuard.publicKey}`)
-            for (const group of candyGuard.groups) {
-                console.log(`groupLabel: ${group.label}`)
-                console.log("addressGate:")
-                console.log(group.guards.addressGate)
-                console.log("allocation:")
-                console.log(group.guards.allocation)
-                console.log("solPayment:")
-                console.log(group.guards.solPayment)
-                console.log("startDate:")
-                console.log(group.guards.startDate)
-                console.log("nftGate:")
-                console.log(group.guards.nftGate)
-            }
-        }
-    
-        return { candyMachine, candyGuard }
+    async function fetchCandyMachine() {
+        const fetched = await candyMachines
+        .findByAddress({ address: new PublicKey(process.env.NEXT_PUBLIC_CANDY_MACHINE_KEY) })
+
+        console.log("Fetched candy machine!", fetched)
+        setCandyMachine(fetched)
     }
 
     useEffect(() => {
-        fetchCandyMachineAndGuard(process.env.NEXT_PUBLIC_CANDY_MACHINE_KEY, true);
+        fetchCandyMachine()
     }, [])
 
-    return null;
+    async function mintOne() {
+        setIsMinting(true);
+    
+        const mintOutput = await candyMachines
+          .mint({ candyMachine });
+    
+        setIsMinting(false);
+        console.log("Minted one!", mintOutput)
+    
+        // Fetch the candy machine to update the counts
+        await fetchCandyMachine()
+      }
+    
+      const canMint =
+        candyMachine &&
+        candyMachine.itemsRemaining.toNumber() > 0 &&
+        wallet.publicKey &&
+        !isMinting
+
+    return (
+        <button
+          type="button"
+          className="inline-flex items-center px-4 py-2 text-sm font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md cursor-pointer hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
+          disabled={!canMint}
+          onClick={mintOne}
+        >
+          Mint 1 {candyMachine ? candyMachine.symbol : "NFT"}! <span className={isMinting ? 'animate-spin' : 'animate-none'}>🦖</span>
+        </button>
+    )
 }
 
 export default Tx;
